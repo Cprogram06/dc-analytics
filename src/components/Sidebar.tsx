8e52e1a268de4f2dbbac2eac52985e780b2f37ea
Cprogram06 <creator.program.dev@gmail.com>
2024-11-25 21:38:44 +0800

    Data and Sidebar Update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,14 +18,16 @@ const Sidebar: React.FC<SidebarProps> = ({ setPage }) => {
             <ul>
                 <li><button onClick={() => setPage('Axie Infinity-2024.html')}>Axie Infinity</button></li>
                 <li><button onClick={() => setPage('Apeiron-2024.html')}>Apeiron</button></li>
-                <li><button onClick={() => setPage('CyberKongz-2024.html')}>CyberKongz</button></li>
+                <li><button onClick={() => setPage('⚡ CyberKongz ⚡-2024.html')}>CyberKongz</button></li>
                 <li><button onClick={() => setPage('Forgotten Runes Wizard Cult-2024.html')}>Forgotten Runiverse</button></li>
-                <li><button onClick={() => setPage('Kaidro-2024.html')}>Kaidro</button></li>
+                <li><button onClick={() => setPage('Fableborne-2024.html')}>Fableborne</button></li>
+                <li><button onClick={() => setPage('Official Kaidro Channel-2024.html')}>Kaidro</button></li>
                 <li><button onClick={() => setPage('Lumiterra-2024.html')}>Lumiterra</button></li>
                 <li><button onClick={() => setPage('Moku-2024.html')}>Moku</button></li>
                 <li><button onClick={() => setPage('Pixels-2024.html')}>Pixels</button></li>
+                <li><button onClick={() => setPage('Pixel Heroes Adventure-2024.html')}>Pixel Heroes</button></li>
                 <li><button onClick={() => setPage('PlayFightLeague-2024.html')}>Play Fight League</button></li>
-                <li><button onClick={() => setPage('Ragmon-2024.html')}>Ragnarok Montster World</button></li>
+                <li><button onClick={() => setPage('Ragnarok_ Monster World-2024.html')}>Ragnarok Montster World</button></li>
                 <li><button onClick={() => setPage('Ronin Creator Discord-2024.html')}>Ronin Creator Discord</button></li>
                 <li><button onClick={() => setPage('Ronin Network-2024.html')}>Ronin Network</button></li>
                 <li><button onClick={() => setPage('The Machines Arena-2024.html')}>The Machines Arena</button></li>
